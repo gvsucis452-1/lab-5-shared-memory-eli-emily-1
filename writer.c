c075a5489e147e9eb6ef5464851c4f3ac4abf175
emheyboer <157163774+emheyboer@users.noreply.github.com>
2025-09-25 09:46:27 -0400

    Add writer

--- writer.c
+++ writer.c
@@ -13,13 +13,50 @@
 #include <sys/ipc.h> 
 #include <sys/shm.h>
 
-int main() {
-    char *input;
-    while (1)
-    {
-        input = //fgets();
-    }
-    
+#define SHM_SIZE 4097
+int shmId;
+void* shared;
 
-    return 0;
+void handleSignal(int signal) {
+    printf("Shutting down...\n");
+    if (shmdt (shared) < 0) { 
+        perror ("Unable to detach\n"); 
+        exit (1); 
+    } 
+    if(shmctl (shmId, IPC_RMID, 0) < 0) { 
+        perror ("Unable to deallocate\n"); 
+        exit(1); 
+    }
+    exit(0);
+}
+
+int main(int argc, char* argv[]) {
+    key_t key = ftok("README.md", 'e');
+
+    if((shmId = shmget(key, SHM_SIZE, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) { 
+        perror ("Unable to get shared memory\n"); 
+        exit (1); 
+    } 
+    if((shared = shmat (shmId, 0, 0)) == (void*) -1) { 
+        perror ("Unable to attach\n"); 
+        exit (1); 
+    }
+
+    signal(SIGINT, handleSignal);
+
+    char* message = shared + 1;
+    char* turn = shared;
+    *turn = 0;
+
+    while (1) {
+        if (*turn) {
+            continue;
+        }
+        printf("> ");
+        fgets(message, SHM_SIZE - 1, stdin);
+
+        *turn = 1;
+    }
+
+   return 0;
 }
